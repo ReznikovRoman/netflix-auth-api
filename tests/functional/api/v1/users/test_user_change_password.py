f8c1cf07cc3dc43e9344e17f047daeaa3170de81
Roman Reznikov <romanreznikov2002@yandex.ru>
2022-05-29 17:52:00 +0400

    #11: test: refactor tests for changing user password

--- tests/functional/api/v1/users/test_user_change_password.py
+++ tests/functional/api/v1/users/test_user_change_password.py
@@ -2,39 +2,53 @@ class TestUserChangePassword:
     """Тестирование смены пароля."""
 
     def test_ok(self, anon_client, user_dto):
-        """Пароль пользователя успешно сменился."""
+        """После смены пароля пользователь не может использовать старый access токен."""
         access_token, _ = self._user_login(anon_client, user_dto)
         headers = {"Authorization": f"Bearer {access_token}"}
-        new_password = f"{user_dto.password} change"
+        new_password = f"{user_dto.password}_new"
         body = {
             "old_password": user_dto.password,
             "new_password1": new_password,
             "new_password2": new_password,
         }
 
-        # Сменить пароль
         anon_client.post(
             "/api/v1/users/me/change-password", data=body, headers=headers, expected_status_code=204, as_response=True)
 
-        # Проверить что после смены пароля старый токен перестал работать
         anon_client.post("/api/v1/users/me/change-password", data=body, headers=headers, expected_status_code=401)
 
-        # Авторизоваться с новым паролем
-        user_dto.password = new_password
-        self._login(anon_client, user_dto)
+    def test_can_login_with_new_password(self, anon_client, user_dto):
+        """Пользователь может успешно войти в аккаунт с измененным паролем."""
+        access_token, _ = self._user_login(anon_client, user_dto)
+        headers = {"Authorization": f"Bearer {access_token}"}
+        new_password = f"{user_dto.password}_new"
+        body = {
+            "old_password": user_dto.password,
+            "new_password1": new_password,
+            "new_password2": new_password,
+        }
 
-    def test_wrong_second_password(self, db_session, anon_client, user_dto):
+        anon_client.post(
+            "/api/v1/users/me/change-password", data=body, headers=headers, expected_status_code=204, as_response=True)
+
+        body = {"email": user_dto.email, "password": new_password}
+        credentials = anon_client.post("/api/v1/auth/login", data=body, expected_status_code=200)["data"]
+        assert "access_token" in credentials
+
+    def test_wrong_second_password(self, anon_client, user_dto):
+        """При несовпадающих паролях клиент получит ошибку."""
         access_token, _ = self._user_login(anon_client, user_dto)
         headers = {"Authorization": f"Bearer {access_token}"}
         body = {
             "old_password": user_dto.password,
-            "new_password1": f"{user_dto.password} change",
-            "new_password2": f"{user_dto.password} wrong second pass",
+            "new_password1": f"{user_dto.password}_1",
+            "new_password2": f"{user_dto.password}_2",
         }
 
         got = anon_client.post(
             "/api/v1/users/me/change-password", data=body, headers=headers, expected_status_code=400,
         )["error"]
+
         assert got["code"] == "passwords_mismatch"
 
     def test_invalid_access_token(self, anon_client, user_dto):
