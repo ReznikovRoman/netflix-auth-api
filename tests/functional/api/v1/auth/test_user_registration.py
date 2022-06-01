ee0d5cff51d92e053a10116143b3c06f16867f32
Roman Reznikov <romanreznikov2002@yandex.ru>
2022-06-01 13:57:11 +0400

    #14: test: remove redundant fixtures from `users` tests

--- tests/functional/api/v1/auth/test_user_registration.py
+++ tests/functional/api/v1/auth/test_user_registration.py
@@ -1,10 +1,7 @@
-from users.models import User
-
-
 class TestUserRegistration:
     """Тестирование регистрации пользователей."""
 
-    def test_ok(self, db_session, anon_client, user_dto):
+    def test_ok(self, anon_client, user_dto):
         """При корректном теле запроса пользователь с ролями по умолчанию регистрируется/создается корректно."""
         body = {
             "email": user_dto.email,
@@ -12,14 +9,10 @@ class TestUserRegistration:
         }
         got = anon_client.post("/api/v1/auth/register", data=body)["data"]
 
-        users = db_session.query(User).all()
-
-        assert len(users) == 1
-        assert len(users[0].roles) > 0
         assert got["id"] is not None
         assert got["email"] == user_dto.email
 
-    def test_invalid_body(self, db_session, anon_client):
+    def test_invalid_body(self, anon_client):
         """Если в теле запроса неправильно заполнены поля, то клиент получит соответствующую ошибку."""
         body = {
             "email": "wrongemail.com",
@@ -27,22 +20,17 @@ class TestUserRegistration:
         }
         got = anon_client.post("/api/v1/auth/register", data=body, expected_status_code=400)
 
-        users = db_session.query(User).all()
-
-        assert len(users) == 0
         assert "errors" in got
         assert "email" in got["errors"]
 
-    def test_user_exists(self, db_session, anon_client):
+    def test_user_exists(self, anon_client):
         """Если пользователь уже существует с почтой из формы регистрации, то клиент получит соответствующую ошибку."""
         body = {
             "email": "duplicate@gmail.com",
             "password": "test-password",
         }
         anon_client.post("/api/v1/auth/register", data=body)
+
         got = anon_client.post("/api/v1/auth/register", data=body, expected_status_code=409)  # делаем второй запрос
 
-        users = db_session.query(User).all()
-
-        assert len(users) == 1
         assert got["error"]["code"] == "user_exists"
