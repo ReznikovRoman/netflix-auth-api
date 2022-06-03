c7b371bbf09710d08316160179b2cf18756cee16
Roman Reznikov <romanreznikov2002@yandex.ru>
2022-06-03 22:08:55 +0400

    chore: configure exemption rules for `Flask Limiter`

--- src/throttling.py
+++ src/throttling.py
@@ -5,6 +5,8 @@ from typing import TYPE_CHECKING
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
+from flask import request
+
 from core.config import get_settings
 
 if TYPE_CHECKING:
@@ -12,11 +14,26 @@ if TYPE_CHECKING:
 
 settings = get_settings()
 
+
+def exemption_rules() -> bool:
+    """Исключения из лимитов по умолчанию.
+
+    https://flask-limiter.readthedocs.io/en/stable/configuration.html#RATELIMIT_DEFAULTS_EXEMPT_WHEN
+    """
+    swagger_urls = ("/api/v1/docs", "/api/v1/swagger.json")
+    exempt = (
+        not request.path.startswith("/api/") or
+        request.path in swagger_urls
+    )
+    return exempt
+
+
 limiter = Limiter(
     storage_uri=f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}/{settings.REDIS_THROTTLE_STORAGE_DB}",
     key_prefix=settings.THROTTLE_KEY_PREFIX,
     key_func=get_remote_address,
     default_limits=settings.THROTTLE_DEFAULT_LIMITS,
+    default_limits_exempt_when=exemption_rules,
     headers_enabled=True,
 )
 
